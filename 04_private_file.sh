271c92cee6d8bae76981f204ed78812c3951863d
Magdalena Radecka <magdalenaradecka0@gmail.com>
2022-01-25 15:04:32 +0100

    First approach in Bash

---
+++ 04_private_file.sh
@@ -0,0 +1,2 @@
+touch this_file_is_private
+chmod u=rwx this_file_is_private
---
+++ 05_organise_again/05_organise_again.sh
@@ -0,0 +1,11 @@
+mkdir -p "Immersion week"/"Day 0"
+touch "Immersion week"/"Day 0"/learnings.md
+mkdir -p "Immersion week"/"Day 1"
+touch "Immersion week"/"Day 1"/learnings.md
+mkdir -p "Immersion week"/"Day 2"
+touch "Immersion week"/"Day 2"/learnings.md
+mkdir -p "Immersion week"/"Day 3"
+touch "Immersion week"/"Day 3"/learnings.md
+mkdir -p "Immersion week"/"Day 4"
+touch "Immersion week"/"Day 4"/learnings.md
+echo Done
